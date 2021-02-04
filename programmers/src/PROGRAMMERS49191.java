64116b5f3f6d7aaf3a26d30eb670405e9d801945
hiheehee <gkek41@naver.com>
2021-02-04 22:12:50 +0900

    2021/02/04 commit

--- programmers/src/PROGRAMMERS49191.java
+++ programmers/src/PROGRAMMERS49191.java
@@ -9,7 +9,7 @@ public class PROGRAMMERS49191 {
 		int answer = 0;
 		int map[][] = new int[n+1][n+1];
 		for(int i = 0; i < n+1; i++) {
-			Arrays.fill(map[i], 1000000);
+			Arrays.fill(map[i], Integer.MAX_VALUE);
 		}
 		for(int i = 0; i < results.length; i++) {
 			map[results[i][0]][results[i][1]] = 1;
@@ -18,29 +18,17 @@ public class PROGRAMMERS49191 {
 		for(int k = 1; k < n+1; k++) {
 			for(int i = 1; i < n+1; i++) {
 				for(int j = 1; j < n+1; j++) {
+					if(i == j) map[i][j] = 0;
+					if(map[i][k] == Integer.MAX_VALUE || map[k][j] == Integer.MAX_VALUE) continue; 
 					map[i][j] = Math.min(map[i][j], map[i][k] + map[k][j]);
 				}
 			}
 		}
 		
 		for(int i = 1; i < n+1; i++) {
-			for(int j = 1; j < n+1; j++) {
-				if(map[i][j] == Integer.MAX_VALUE) {
-					System.out.print(0+" ");
-				}else {
-					System.out.print(map[i][j]+" ");
-				}
-				
-			}
-			System.out.println();
-		}
-		System.out.println();
-		
-		for(int i = 1; i < n+1; i++) {
 			boolean check = true;
 			for(int j = 1; j < n+1; j++) {
-				if(i == j) continue;
-				if(map[i][j] == 1000000 || map[j][i] == 1000000) {
+				if(map[i][j] == Integer.MAX_VALUE && map[j][i] == Integer.MAX_VALUE) {
 					check = false;
 					break;
 				}
@@ -49,6 +37,8 @@ public class PROGRAMMERS49191 {
 				answer++;
 			}
 		}
+		
+		System.out.println(answer);
 	}
 
 }
