4c175edce42ba9c1a638ff19b823b4786aef1cd0
hiheehee <gkek41@naver.com>
2021-08-06 10:38:40 +0900

    2개 이하로 다른 비트

--- programmers/src/PROGRAMMERS77885.java
+++ programmers/src/PROGRAMMERS77885.java
@@ -1,9 +1,6 @@
-
 public class PROGRAMMERS77885 {
-
-	public static void main(String[] args) {
-		long[] numbers = {5};
-		long[] answer = new long[numbers.length];
+    public long[] solution(long[] numbers) {
+        long[] answer = new long[numbers.length];
         int n = 100000;
 		
         for(int i = 0; i < answer.length; i++){
@@ -13,28 +10,20 @@ public class PROGRAMMERS77885 {
             	boolean check = false;
                 String str = Long.toBinaryString(numbers[i]);
                 for(int j = str.length()-1; -1 < j; j--) {
-                	//System.out.println(str.charAt(j));
                 	if(str.substring(j, j+1).equals("0")) {
                 		check = true;
                 		String tmp = str.substring(0, j)+"10"+str.substring(j+2);
                 		str = tmp;
-                		System.out.println(str.substring(0, j));
-                		System.out.println(10);
-                		System.out.println(str.substring(j+2));
                 		break;
                 	}
                 }
-                
                 if(!check) {
                 	str = "10" + str.substring(1);
                 }
-                //System.out.println(str);
-           
+                answer[i] = Long.parseLong(str,2);
             }
             
         }
-        //return answer;
-
-	}
-
+        return answer;
+    }
 }
